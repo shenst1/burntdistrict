bd7d994d518b225e6aa1471b8ff83640aa157f1f
Andrew Shenstone <shenst1@gmail.com>
2015-01-18 20:25:55 -0800

    Add map link that hopefully works on mobile.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,4 +11,17 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
   $('[data-toggle="popover"]').popover();
+  function navigation() {
+    // If it's an iPhone..
+    console.log("Something")
+    if( (navigator.platform.indexOf("iPhone") != -1)
+        || (navigator.platform.indexOf("iPod") != -1)
+        || (navigator.platform.indexOf("iPad") != -1))
+         window.open("maps://maps.google.com/maps/place/Burnt+District+Salon+%26+Spa/@47.603283,-122.33513,17z/data=!3m1!4b1!4m2!3m1!1s0x54906ab015d3a017:0xcea7e7e86d66178");
+    else
+         window.open("https://www.google.com/maps/place/Burnt+District+Salon+%26+Spa/@47.603283,-122.33513,17z/data=!3m1!4b1!4m2!3m1!1s0x54906ab015d3a017:0xcea7e7e86d66178");
+  }
+  $( "#Map" ).on( "click", function() {
+    navigation();
+  });
 })
